38c170f186fb4251402017e0eae79012ad989d5f
Prasad Sawant <prasadsawant5@gmail.com>
2018-08-23 16:58:30 -0700

    persistency improved

--- routes/index.js
+++ routes/index.js
@@ -29,166 +29,37 @@ router.post('/addItem', function (req, res, next) {
       res.status(201).json({ message: 'Bin updated', obj: bin });
     });
   } else {
-    Bin.update({ name: body.bin }, { $set: { items: body.items } }, function (err, bin) {
+    Bin.updateMany({ items: { $in: body.items } }, { $pull: { items: body.items }}, function(err, bins) {
       if (err) {
         console.error(err);
         return res.status(400).json({
-          message: 'DB error while updating the bin.',
+          message: 'DB error while deleting bins with duplicate items.',
           obj: err
         });
       }
 
-      if (bin === null) {
-        return res.status(400).json({
-          message: 'DB error while updating the bin.'
-        });
-      }
+      console.log(bins);
 
-      res.status(201).json({ message: 'Bin updated', obj: bin });
+      Bin.update({ name: body.bin }, { $set: { items: body.items } }, function (err, bin) {
+        if (err) {
+          console.error(err);
+          return res.status(400).json({
+            message: 'DB error while updating the bin.',
+            obj: err
+          });
+        }
+  
+        if (bin === null) {
+          return res.status(400).json({
+            message: 'DB error while updating the bin.'
+          });
+        }
+  
+        res.status(201).json({ message: 'Bin updated', obj: bin });
+      });
+
     });
   }
-
-  // if ((body.bin.includes('bin') || body.bin.includes('up') || body.bin.includes('Object') || body.bin.includes('com')) && 
-  // (body.items.indexOf('bin') === -1 && body.items.indexOf('up') === -1 && body.items.indexOf('Object') === -1 && body.items.indexOf('com') === -1)) {
-  //   Bin.findOne({ name: body.bin }, function(err, bin) {
-  //     if (err) {
-  //       console.error(err);
-  //       return res.status(400).json({
-  //         message: 'DB error while finding a bin.',
-  //         obj: err
-  //       });
-  //     }
-
-  //     if (bin === null) {
-  //       return res.status(400).json({
-  //         message: 'Cannot finding the bin.'
-  //       });
-  //     }
-
-  //     if (new Date() - bin.updatedAt > 60000 * 2) {
-  //       bin.items = body.items;
-  //       bin.save();
-  //     }
-
-  //     res.status(201).json({ message: 'Bin updated', obj: bin });
-  //   });
-  // } else if ((body.bin.includes('bin') || body.bin.includes('up') || body.bin.includes('Object') || body.bin.includes('com')) && 
-  // (body.items.indexOf('bin') !== -1 || body.items.indexOf('up') !== -1 || body.items.indexOf('Object') !== -1 || body.items.indexOf('com') !== -1 || body.items === null)) {
-  //   Bin.update({ name: body.bin }, {$set: {items: null}}, function(err, bin) {
-  //     if (err) {
-  //       console.error(err);
-  //       return res.status(400).json({
-  //         message: 'DB error while updating the bin.',
-  //         obj: err
-  //       });
-  //     }
-
-  //     if (bin === null) {
-  //       return res.status(400).json({
-  //         message: 'DB error while updating the bin.'
-  //       });
-  //     }
-
-  //     res.status(201).json({ message: 'Bin updated', obj: bin });
-  //   });
-
-  // }
-
-  // Bin.find({ items: { $in: [body.item] } }, function (erFind, bins) {
-  //   if (bins !== []) {
-  //     bins.forEach(function (ele) {
-  //       if (ele.name !== body.bin.toUpperCase()) {
-  //         if (ele.items.length > 1) {
-  //           // update
-  //           Bin.update({ name: ele.name }, { $pull: { items: [body.item] } });
-  //         } else {
-  //           console.log('delete');
-  //           Bin.deleteOne({ name: ele.name }, function (delEr, del) {
-  //             if (delEr) {
-  //               console.error(delEr);
-  //             }
-  //           });
-  //         }
-  //       }
-  //     });
-  //   }
-  // });
-
-  // Bin.find({}, function (er, bins) {
-  //   if (er) {
-  //     console.error(er);
-  //     return res.status(400).json({
-  //       message: 'Error while finding an existing bin.',
-  //       obj: err
-  //     });
-  //   }
-  //   if (bins !== []) {
-  //     console.log('finding all bins');
-  //     bins.forEach(function (ele) {
-  //       if (ele.items.includes(body.item)) {
-  //         console.log(ele.name);
-  //         console.log(body.bin);
-  //         if (ele.name !== body.bin.toUpperCase()) {
-  //           if (ele.items.length > 1) {
-  //             // update
-  //             Bin.update({ name: ele.name }, { $pull: { items: [body.item] } });
-  //           } else {
-  //             console.log('delete');
-  //             Bin.deleteOne({ name: ele.name }, function (delEr, del) {
-  //               if (delEr) {
-  //                 console.error(delEr);
-  //               }
-  //             });
-  //           }
-  //         }
-  //       }
-  //     });
-  //   }
-  // });
-
-  // Bin.findOne({ name: body.bin }, function (e, b) {
-  //   if (e) {
-  //     console.error(err);
-  //     return res.status(400).json({
-  //       message: 'Not able to find bin.',
-  //       obj: err
-  //     });
-  //   }
-
-  //   if (b === null) {
-  //     bin.save(function (err, data) {
-  //       if (err) {
-  //         console.error(err);
-  //         return res.status(400).json({
-  //           message: 'Not able to save bin.',
-  //           obj: err
-  //         });
-  //       }
-
-  //       if (data === null) {
-  //         return res.status(400).json({
-  //           message: 'Not able to find item.',
-  //           obj: null
-  //         });
-  //       }
-
-  //       res.status(201).json({ 'message': 'OK' });
-  //     });
-  //   } else {
-  //     console.log('bin already exists');
-  //     var items = b.items;
-
-  //     if (items.includes(body.item)) {
-  //       console.log('Item already exists');
-  //     } else {
-  //       b.items.push(body.item);
-  //       b.save();
-  //     }
-  //     res.status(201).json({ 'message': 'OK' });
-  //   }
-  // });
-
-  // res.status(201).json({ 'message': 'OK'});
 });
 
 
